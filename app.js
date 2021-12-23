de6ed299c8bdbffd7769f1e0fb1da2f84ef5f379
Thiago <thiagotresca@hotmail.com>
2021-12-22 23:46:56 -0300

    adiciona lógica de sort ao código

--- app.js
+++ app.js
@@ -1,33 +1,78 @@
-const array = 
-[
-{
-"ID": 1,
-"Descricao": "Importacao de arquivos de fundos",
-"Data Maxima de conclusao": "2019-11-10 12:00:00",
-"Tempo estimado": "2 horas",
-},
-{
-"ID": 2,
-"Descricao": "Importacao de dados da Base Legada",
-"Data Maxima de conclusao": "2019-11-11 12:00:00",
-"Tempo estimado": "4 horas",
-},
-{
-"ID": 3,
-"Descricao": "Importacao de dados de integracao",
-"Data Maxima de conclusao": "2019-11-11 08:00:00",
-"Tempo estimado": "6 horas",
-},
-]
+const array = [
+    {
+    "ID": 2,
+    "Descrição": "Importação de dados da Base Legada",
+    "Data Maxima de conclusao": "2019-11-11 12:00:00",
+    "Tempo estimado de execucao em horas": 4,
+    },
+    {
+    "ID": 1,
+    "Descrição": "Importação de arquivos de fundos",
+    "Data Maxima de conclusao": "2019-11-10 12:00:00",
+    "Tempo estimado de execucao em horas": 2,
+    },
+    {
+    "ID": 3,
+    "Descrição": "Importação de dados de integração",
+    "Data Maxima de conclusao": "2019-11-11 08:00:00",
+    "Tempo estimado de execucao em horas": 6,
+    },
+  ]
+  
+  const dataMinima = new Date("2019-11-10 09:00:00")
+  const dataMaxima = new Date("2019-11-11 12:00:00")
+  
+  let arrayFinal = []
+  function calculaTempoExecucaoJobs (jobsSorteados) {
 
-const dataMinima = new Date("2019-11-10 09:00:00")
-const dataMaxima = new Date("2019-11-11 12:00:00")
-function returnJobs() {
-    array.map((item) => {
+    let jobsSorteadosCopia = [...jobsSorteados]
+    let contadorDeHoras = 0
+    let arrayTemporario = []
+  
+    jobsSorteadosCopia.map( (job, index) => {
+      contadorDeHoras = contadorDeHoras + job['Tempo estimado de execucao em horas']
+  
+      arrayTemporario.push(job)
+  
+      if(contadorDeHoras < 8) {
+        jobsSorteadosCopia.splice(index, 1)
+        return false
+      }
+  
+      if(contadorDeHoras == 8) {
+        jobsSorteadosCopia.splice(index, 1)
+        arrayFinal.push(arrayTemporario)
+        calculaTempoExecucaoJobs(jobsSorteadosCopia)
+      }
+  
+      if(contadorDeHoras > 8) {
+        arrayTemporario.pop()
+        arrayFinal.push(arrayTemporario)
+        calculaTempoExecucaoJobs(jobsSorteadosCopia)
+      }
+    })
+    console.log(arrayFinal)
+}
+
+  function returnJobs() {
+    // verifica se o job está dentro do periodo de execução
+    let jobsDentroDoPeriodo = array.map((item) => {
       let data = new Date(item["Data Maxima de conclusao"])
   
       if(data >= dataMinima && data <= dataMaxima) {
-        console.log('esta dentro do limite')
+        return item
       }
     })
+  
+    // faz um sort dos jobs de acordo com a data limite de conclusão
+    let jobsSorteados = jobsDentroDoPeriodo.sort((a,b)=>{
+      return new Date(a["Data Maxima de conclusao"]) - new Date(b["Data Maxima de conclusao"]);
+    });
+  
+    calculaTempoExecucaoJobs(jobsSorteados)
+  
+  }
+  function chamaFuncoes() {
+    returnJobs()
+    return arrayFinal
 }
